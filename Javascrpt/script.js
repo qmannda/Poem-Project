13b58c7a8a012a9b76491fb99fe1cf17e6ed7687
qmannda <qmannda@gmail.com>
2025-01-17 00:41:01 +0200

    Added AI api

--- Javascrpt/script.js
+++ Javascrpt/script.js
@@ -1,12 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "Shall I compare thee to a summers day?",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "e3baoc44e6718daa5fte65cbd6d06a5b";
+  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+  let context =
+    "You are a peom expert and love to write short poems. Your mission is to generate a 4 line poem in html form without ```html. Make sure to follow user instructions";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
